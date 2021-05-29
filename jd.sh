f82ad5d35ba99bdfeea57b3eb10a4b018dbac1ba
Lan <admin@highdimen.com>
2021-05-29 10:03:32 -0400

    更新于 05-29 10:03   fix

--- jd.sh
+++ jd.sh
@@ -135,7 +135,7 @@ function Combin_All() {
   ## 京东手机狂欢城(jd_carnivalcity.js)
   export JD818_SHARECODES=$(Combin_Sub ForOtherCarnivalcity "5443fec1-7dbc-4d92-a09b-b7eb0a01199f@8c2a0d3a-b4d7-4bbf-bccc-4e7efc18f849")
   ## 城城分现金(jd_city.js)
-  export CITY_SHARECODES=$(Combin_Sub ForOtherCity "RtGKzu6kEw-jeNeaENw73x4aTHtnmBBm8KSerYO5pD1jnXS9hg@RtGKiarVGFDwIvfzQpx5mlnLn7PlHH-WBmLyPXQ4GKO_uHvx@RtGKk6ndHkPCIcbCZKhumm2vdM7DL5DWnLF4QTmGxh_ezmKF")
+  export CITY_SHARECODES=$(combine_sub ForOtherCity "RtGKzu6kEw-jeNeaENw73x4aTHtnmBBm8KSerYO5pD1jnXS9hg@RtGKiarVGFDwIvfzQpx5mlnLn7PlHH-WBmLyPXQ4GKO_uHvx@RtGKk6ndHkPCIcbCZKhumm2vdM7DL5DWnLF4QTmGxh_ezmKF@XYHgyOSjSReqf4GTFdx_mnGTeu_gdHr-iPknY4CxfUkKuiI")
   ## 618动物联萌(jd_zoo.js)
   export ZOO_SHARECODES=$(Combin_Sub ForOtherZoo)
 }
