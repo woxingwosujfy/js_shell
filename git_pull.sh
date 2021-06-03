a2b5996d786140d0fdfe5a330ddab6d773f48e64
Lan <admin@highdimen.com>
2021-06-03 01:41:34 -0400

    更新于 06-03 01:41

--- git_pull.sh
+++ git_pull.sh
@@ -110,8 +110,8 @@ function Update_Cron() {
     done
     perl -i -pe "s|.+(bash.+git_pull.+log.*)|${RanMin} ${RanHour} \* \* \* sleep ${RanSleep} && \1|" ${ListCron}
 
-    perl -i -pe "s|.*(.+jd_zooCollect\W*.*)|20,40 \* \* \* \* \1|" ${ListCron}
-    perl -i -pe "s|.*(.+jd_zoo\W*.*)|1 \* \* \* \* \1|" ${ListCron}
+    perl -i -pe "s|.*(.+jd_zooCollect \W*.*)|20,40 \* \* \* \* \1|" ${ListCron}
+    perl -i -pe "s|.*(.+jd_zoo \W*.*)|1 \* \* \* \* \1|" ${ListCron}
 
     crontab ${ListCron}
   fi
