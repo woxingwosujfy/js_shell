913a5c1f8abd542d6ef44815aff1c20b63880bcd
Lan <admin@highdimen.com>
2021-06-03 02:23:15 -0400

    更新于 06-03 02:23

--- git_pull.sh
+++ git_pull.sh
@@ -222,14 +222,14 @@ function Diff_Cron {
 ## 发送删除失效定时任务的消息
 function Notify_DropTask {
   cd ${ShellDir}
-  node update.js
+  #node update.js
   [ -f ${ContentDropTask} ] && rm -f ${ContentDropTask}
 }
 
 ## 发送新的定时任务消息
 function Notify_NewTask {
   cd ${ShellDir}
-  node update.js
+  #node update.js
   [ -f ${ContentNewTask} ] && rm -f ${ContentNewTask}
 }
 
